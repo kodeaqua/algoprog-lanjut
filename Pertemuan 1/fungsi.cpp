3bcc3caa808d77e005e3b4e22ef4b5226c936171
Adam Najmi Zidan <33061778+kodeaqua@users.noreply.github.com>
2021-03-07 19:38:39 +0700

    Upload source code pertemuan #1 tahun lalu

---
+++ Pertemuan 1/fungsi.cpp
@@ -0,0 +1,16 @@
+#include iostream
+#inclide conio.h
+using namespace std;
+ pembuatan fungsi garis()  
+void garis()
+{
+coutn ---------------------- n;
+}
+ program utama 
+int main()
+{
+garis();
+coutUNIVERSITAS PAKUAN BOGORendl;
+garis();
+	getch();
+}
